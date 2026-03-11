158c0abeca60e12ec242c122876b5b2d442db17e
Robert Marko <robert.marko@sartura.hr>
2026-03-11 18:12:03 +0100

    mvebu: cortexa53: uDPU/eDPU: add special migration sysupgrade for eMMC images
    Add a custom recipe for uDPU/eDPU that will wipe eMMC, then write the new GPT
    image for new eMMC based images.
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- target/linux/mvebu/cortexa53/base-files/lib/upgrade/platform.sh
+++ target/linux/mvebu/cortexa53/base-files/lib/upgrade/platform.sh
@@ -7,6 +7,25 @@ RAMFS_COPY_BIN='fw_printenv fw_setenv mkfs.f2fs fdisk'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 REQUIRE_IMAGE_METADATA=1
 
+udpu_check_emmc() {
+	# Check which device is available, depending on the board revision
+	if [ -b "/dev/mmcblk1" ]; then
+		emmc_dev=/dev/mmcblk1
+	elif [ -b "/dev/mmcblk0" ]; then
+		emmc_dev=/dev/mmcblk0
+	else
+		echo "Cannot detect eMMC flash, aborting.."
+		exit 1
+	fi
+}
+
+platform_do_upgrade_uDPU() {
+	udpu_check_emmc
+
+	dd if=/dev/zero of=${emmc_dev} bs=1M
+	dd if="$1" of=${emmc_dev}
+}
+
 platform_check_image() {
 	case "$(board_name)" in
 	glinet,gl-mv1000|\
@@ -54,7 +73,6 @@ platform_copy_config() {
 		;;
 	methode,udpu|\
 	methode,edpu)
-		platform_copy_config_uDPU
 		;;
 	esac
 }
